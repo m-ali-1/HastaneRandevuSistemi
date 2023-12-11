30f4a996bee846af2a41000e643ad1968e447bd1
m-ali-1 <yla.8421.yla@gmail.com>
2023-12-11 21:52:22 +0300

    Proje dosyası ekle.

---
+++ Hastane.Repositories/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Hastane.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hastane.Repositories
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<ApplicationUser>ApplicationUsers { get; set; }
+    }
+}
---
+++ Hastane.Utilities/DbInitializier.cs
@@ -0,0 +1,61 @@
+﻿using Hastane.Models;
+using Hastane.Repositories;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hastane.Utilities
+{
+    public class DbInitializier : IDbInitializier
+    {
+        private UserManager<IdentityUser> _userManager;
+        private RoleManager<IdentityRole> _roleManager;
+        private ApplicationDbContext _context;
+
+        public DbInitializier(UserManager<IdentityUser> userManager, 
+            RoleManager<IdentityRole> roleManager, 
+            ApplicationDbContext context)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _context = context;
+        }
+
+        public void Initialize()
+        {
+            try 
+            {
+                if (_context.Database.GetPendingMigrations().Count() > 0)
+                {
+                    _context.Database.Migrate();
+                }
+            }
+            catch(Exception)
+            {
+                throw;
+            }
+            if (!_roleManager.RoleExistsAsync(WebSiteRoles.WebSite_Admin).GetAwaiter().GetResult()) 
+            {
+                _roleManager.CreateAsync(new IdentityRole(WebSiteRoles.WebSite_Admin)).GetAwaiter().GetResult();
+                _roleManager.CreateAsync(new IdentityRole(WebSiteRoles.WebSite_Hasta)).GetAwaiter().GetResult();
+
+                _userManager.CreateAsync(new ApplicationUser
+                {
+                    UserName = "Ali",
+                    Email = "ali@denemeali.com"
+                },"Ali@123").GetAwaiter().GetResult();
+
+                var AppUser = _context.ApplicationUsers.FirstOrDefault(x => x.Email == "ali@denemeali.com");
+                if (AppUser != null) 
+                {
+                    _userManager.AddToRoleAsync(AppUser, WebSiteRoles.WebSite_Admin).GetAwaiter().GetResult();
+                }
+            }
+
+        }
+    }
+}
